f267db264bbdf8ee70f5d5a1a14a233403d39cb5
vivek84505 <vivek84505@gmail.com>
2020-10-24 09:57:22 +0530

    Revert "updated config.php"
    This reverts commit eec3b079b780ed23eb437f7a6e1701c05bf90bd0.

--- application/config/config.php
+++ application/config/config.php
@@ -384,10 +384,10 @@ $config['encryption_key'] = 'and2015';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';
+$config['sess_driver'] = 'files';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = 'ci_sessions';
+$config['sess_save_path'] = 'sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 7200;
 $config['sess_regenerate_destroy'] = TRUE;
@@ -410,7 +410,7 @@ $config['sess_regenerate_destroy'] = TRUE;
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= TRUE;
+$config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= FALSE;
 
 /*
